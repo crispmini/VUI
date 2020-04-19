2660b79e6bab873e1af0f5b6d0800482b21dab3a
chrisfloomis <christopherfrenchloomis@gmail.com>
2020-04-19 13:42:09 -0400

    JS functions to work tabs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /* JS Document */
 
-function init(){
+function init() {
+  var i, ticketLinks, ticket, ticketContentLinks, ticketContent;
+
   ticketLinks = document.getElementsByClassName('ticket-links');
   ticket = document.getElementsByClassName('ticket');
   ticketContentLinks = document.getElementsByClassName('ticket-content-links');
@@ -10,32 +12,56 @@ function init(){
   ticket[0].style.display = "block";
   ticketContentLinks[0].className += " active";
   ticketContent[0].style.display = "block";
+
+  document.getElementById("ticket1").addEventListener("click", function(){ tabTicket("ticket1")});
+  document.getElementById("ticket2").addEventListener("click", function(){ tabTicket("ticket2")});
+  document.getElementById("ticket3").addEventListener("click", function(){ tabTicket("ticket3")});
+
+  document.getElementById("message").addEventListener("click", function(){ tabContent("message")});
+  document.getElementById("contact-info").addEventListener("click", function(){ tabContent("contact-info")});
+  document.getElementById("faq").addEventListener("click", function(){ tabContent("faq")});
+}
+
+function tabTicket(ticket) {
+  ticketLinks = document.getElementsByClassName('ticket-links');
+  
+  for (i = 0; i < ticketLinks.length; i++) {
+    ticketLinks[i].className = ticketLinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(ticket).className += " active";
+
+  tabContent("message");
+}
+
+function tabContent(content) {
+  ticketContentLinks = document.getElementsByClassName('ticket-content-links');
+
+  for (i = 0; i < ticketContentLinks.length; i++) {
+    ticketContentLinks[i].className = ticketContentLinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(content).className += " active";
+
+  openContent(content+"-d");
+}
+
+function openContent(content) {
+  ticketContent = document.getElementsByClassName('ticket-content');
+
+  for (i = 0; i < ticketContent.length; i++) {
+    ticketContent[i].style.display = "none";
+  } 
+
+  document.getElementById(content).style.display = "block";
+}
+
+function newTicket() {
+  console.log("I would initialize a new ticket in the DB and display it for editting. Would result in either a new page or pop-up form.");
+}
+
+function msgLoad() {
+
 }
 
 window.onload = init;
-
-function openTab(evt, tabs, content, tabName) {
-    var i, tabcontent, tablinks;
-
-    tabcontent = document.getElementsByClassName(content);
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    tablinks = document.getElementsByClassName(tabs);
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    
-    document.getElementById(tabName).style.display = "block";
-    console.log(evt.currentTarget.className);
-    evt.currentTarget.className += " active";
-  }
-
-  function newTicket() {
-    console.log("I would initialize a new ticket in the DB and display it for editting. Would result in either a new page or pop-up form.");
-  }
-
-  function msgLoad() {
-
-  }
